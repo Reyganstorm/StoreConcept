71b93566ca5afb4f78ce3fe03c78e6e35be19b1e
Reyganstorm <ruslan.shtybaev@gmail.com>
2022-12-08 13:59:29 +0300

    Initial Commit

---
+++ StoreConcept/StoreConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreConceptApp.swift
+//  StoreConcept
+//
+//  Created by Руслан Штыбаев on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
